c92be3eceecfab5f624d46c3eae02e190a81dd9b
= <roberta.btt@gmail.com>
2019-02-10 21:25:13 +0100

    Python OddOccurrencesInArray

---
+++ src/OddOccurrencesInArray.py
@@ -0,0 +1,22 @@
+__author__ = 'roby'
+
+class OddOccurencyInArray:
+
+
+    def solution(A):
+        # write your code in Python 3.6
+
+        values_map = dict()
+
+        for a in A:
+            if a in values_map:
+                values_map[a] += 1
+            else:
+                values_map[a] = 1
+
+        result = [num for num in values_map.items() if num[1] % 2 == 1][0][0]
+
+        return result
+
+OddOccurencyInArray.solution([9, 3, 9, 3, 9, 7, 9])
+
