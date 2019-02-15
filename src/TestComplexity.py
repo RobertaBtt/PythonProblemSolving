44f2b19e33360539fadcb137a866cadcd184f71b
RobertaBtt <roberta.buttau@gotouche.com>
2019-02-15 15:17:03 +0100

    A class to test the computation time required to solve a small problem

---
+++ src/TestComplexity.py
@@ -0,0 +1,41 @@
+__author__ = 'roby'
+#Problem: You are given an integer n.
+# Count the total of 1+2+ ... + n.
+
+from timeit import default_timer as timer
+
+class TestComplexity:
+    def count_total(n):
+        total = 0
+
+        for i in range(n, 0, -1):
+            total +=i
+
+        return total
+
+    def model_solution(n):
+
+        result = n * (n + 1)//2
+
+        return result
+
+start = timer()
+print("FIRST" , TestComplexity.count_total(100))
+end = timer()
+print((end - start)*100)# Time in seconds, e.g. 5.3809195
+
+start = timer()
+print("SECOND" , TestComplexity.model_solution(100))
+end = timer()
+print((end - start)*100) # Time in seconds, e.g. 5.3809195
+
+#I print *100 just to compare "human readable" numbers, without the exponential notation.
+
+
+
+
+
+
+
+# ...
+
