8154eaba4357b93d9a36ec3ab35aa1e238dd9a2a
= <roberta.btt@gmail.com>
2019-02-15 20:08:55 +0100

    Python PermMissingElem

---
+++ src/PermMissingElem.py
@@ -0,0 +1,14 @@
+__author__ = 'roby'
+
+
+class PermMissingElem:
+
+    def solution(A):
+
+        n = len(A)+1
+        result = n * (n + 1)//2
+
+        return result - sum(A)
+
+
+print(PermMissingElem.solution([1,2,3,5,6,7,8,4]))
