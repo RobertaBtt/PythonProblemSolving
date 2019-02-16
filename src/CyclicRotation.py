a272781d24f7ffda84fab33bfdbfc7556c61a3e3
= <roberta.btt@gmail.com>
2019-02-16 09:55:06 +0100

    Little changes on cyclicRotation

--- src/CyclicRotation.py
+++ src/CyclicRotation.py
@@ -4,6 +4,9 @@ __author__ = 'roby'
 class CyclicRotation:
 
     def solution(A, K):
+        num = 7
+        mylist = [num for num in range(100)]
+        print(mylist)
 
         new_list = []
         reverse_index = 0
@@ -16,12 +19,7 @@ class CyclicRotation:
             new_list.extend(A[0:K-1])
 
 
-
-
-
-
         return new_list
-
 
 
 print (CyclicRotation.solution([3, 8, 9, 7, 6], 3))
