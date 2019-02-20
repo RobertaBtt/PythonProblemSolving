3144d236e36f225978272ded5a3265b7c5344a92
RobertaBtt <roberta.buttau@gotouche.com>
2019-02-20 12:11:29 +0100

    Java ParserFacade

--- src/ParserFacade.java
+++ src/ParserFacade.java
@@ -1,3 +1,5 @@
+package src;
+
 import java.io.*;
 import java.util.Optional;
 
@@ -6,7 +8,12 @@ import java.util.Optional;
  */
 public class ParserFacade {
 
+    private File file;
+    private boolean checkDimension = true;
+    private int maxDimension = 0x80;
+
     private static ParserFacade instance;
+    private ParserFacade(){}
 
     public static ParserFacade getInstance() {
         if (instance == null) {
@@ -14,9 +21,6 @@ public class ParserFacade {
         }
         return instance;
     }
-
-    private ParserFacade(){}
-    private File file;
 
     public synchronized void setFile(File f) {
         file = f;
@@ -31,25 +35,51 @@ public class ParserFacade {
     }
 
     public String getContent() throws IOException {
-        FileInputStream i = new FileInputStream(file);
+        FileInputStream fileInputStream = null;
         String output = "";
         int data;
-        while ((data = i.read()) > 0) output += (char) data;
-        return output;
-    }
-    public String getContentWithoutUnicode() throws IOException {
-        FileInputStream i = new FileInputStream(file);
-        String output = "";
-        int data;
-        while ((data = i.read()) > 0) if (data < 0x80) {
-            output += (char) data;
+
+        try{
+            fileInputStream = new FileInputStream(file);
         }
+        catch (FileNotFoundException ex){}
+
+        if (fileInputStream != null){
+            while ((data = fileInputStream.read()) > 0){
+                if (checkDimension){
+                    /*Appending data only if data is  maxDimension*/
+                    if (data < maxDimension){
+                        output += (char) data;
+                    }
+
+                }
+                else{
+                    /*Appending data without checking maxDimension*/
+                    output += (char) data;
+                }
+
+            }
+        }
+
+
         return output;
     }
+
+
     public void saveContent(String content) throws IOException {
-        FileOutputStream o = new FileOutputStream(file);
-        for (int i = 0; i < content.length(); i += 1) {
-            o.write(content.charAt(i));
+
+        FileOutputStream o = null;
+        try{
+            o = new FileOutputStream(file);
         }
+        catch (FileNotFoundException ex){ }
+        if (o != null){
+
+            for (int i = 0; i < content.length(); i += 1) {
+                o.write(content.charAt(i));
+            }
+        }
+
+
     }
 }
